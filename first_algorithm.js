4dd6a88c472b62d0982ce0ae0643ee631774668b
freddy Castuera <freddycastuera@gmail.com>
2022-01-14 17:29:50 -0700

    terminamos los dos algoritmos en js

--- first_algorithm.js
+++ first_algorithm.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 //esta funcion remueve los caracteres adjacentes duplicados
 function remove_adyacent_duplicates(string){
     const without_adyacent_duplicates = Array.from(string).reduce((acum,char,index,arr)=>{
@@ -11,27 +12,32 @@ function remove_adyacent_duplicates(string){
   //esta funcion verifica si un string (una vez removidos los caracteres adyacentes duplicados) contiene un substring
   function verifyInclusion(string,substring){
     const without_adyacent_duplicates = remove_adyacent_duplicates(string)
-    return without_adyacent_duplicates.includes(substring)
+    return without_adyacent_duplicates.includes(substring)?'si':'no'
   }
 
-  const fs = require('fs')
+function hiddenInstruction(file){
+  fs.access(`./messages/${file}`,err=>{
+      if(err){
+          throw new Error('no encontramos el archivo que desea abrir')
+      }
+      console.log('archivo encontrado correctamente')
+      fs.readFile(`./messages/${file}`,'utf-8',(err,data)=>{
+          if(err){
+              throw new Error('ocurrio un error al leer el archivo')
+          }
+          const formatedData = data.split('\n').slice(1)
+          const firstInstruction = formatedData[0]
+          const secondInstruction = formatedData[1]
+          const message = formatedData[2]
 
-fs.access('./messages/message1.txt',err=>{
-    if(err){
-        throw new Error('no encontramos el archivo que desea abrir')
-    }
-    console.log('archivo encontrado correctamente')
-    fs.readFile('./messages/message1.txt','utf-8',(err,data)=>{
-        if(err){
-            throw new Error('ocurrio un error al leer el archivo')
-        }
-        const formatedData = data.split('\n').slice(1)
-        const firstInstruction = formatedData[0]
-        const secondInstruction = formatedData[1]
-        const message = formatedData[2]
+          const output = verifyInclusion(message,firstInstruction) +'\n' +verifyInclusion(message,secondInstruction)
 
-        console.log(verifyInclusion(message,firstInstruction)?'si':'no')
-        console.log(verifyInclusion(message,secondInstruction)?'si':'no')
+          fs.writeFile(`./messages/results.txt`, output,  (err) => {
+            if (err) throw new Error('ocurrio un error al crear el archivo con los resultados');
+            console.log('Guardamos los resultados correctamente');
+          }); 
 
-    })
-})
+      })
+  })
+}
+hiddenInstruction('message1.txt')
--- second_algorithm.js
+++ second_algorithm.js
@@ -1,29 +1,42 @@
 const fs = require('fs')
+function convertStringIntoObject(data){
+    const formatedData = data.split('\n')
+    const size = parseInt(formatedData[0])
+    //convertimos el arreglo formado por strings("score1 score2") 
+    //a un arreglo compusesto por otros arreglos con los 
+    //valores numericos de dichos scores
+    const scores = formatedData.slice(1,formatedData.length-1).map(item=>item.split(' ').map(player=>parseInt(player)))
+    //convertimos el arreglo en un objecto donde las llaves 
+    //seran el valor absoluto de la diferencias entre los scores
+    // de los dos jugadores
+    const winners = scores.reduce((acum,score)=>{
+        const difference = score[0]-score[1]
+        let winner = (difference>=0)?'1':'2'
+        const absDifference = Math.abs(difference)
+        return {...acum,[absDifference]:[...score,winner]}
+    },{})
+    return winners
+}
 
-fs.access('./scores/score1.txt',err=>{
-    if(err){
-        throw new Error('no encontramos el archivo que desea abrir')
-    }
-    console.log('archivo encontrado correctamente')
-    fs.readFile('./scores/score1.txt','utf-8',(err,data)=>{
+function whoIsTheWinner(file){
+    fs.access(`./scores/${file}`,err=>{
         if(err){
-            throw new Error('ocurrio un error al leer el archivo')
+            throw new Error('no encontramos el archivo que desea abrir')
         }
-        console.log(data.split('\n'))
-        const formatedData = data.split('\n')
-        const size = parseInt(formatedData[0])
-        const scores = formatedData.slice(1,formatedData.length-1).map(item=>item.split(' ').map(player=>parseInt(player)))
-        console.log(size)
-        console.log(scores)
-        const winners = scores.reduce((acum,score)=>{
-            const difference = score[0]-score[1]
-            let winner = (difference>=0)?'1':'2'
-            const absDifference = Math.abs(difference)
-            return {...acum,[absDifference]:[...score,winner]}
-        },{})
-        console.log(winners)
-        const maxDifference = Math.max(...Object.keys(winners))
-        const absoluteWinner = `${winners[maxDifference][2] } ${maxDifference} `
-        console.log(absoluteWinner)
+        console.log('archivo encontrado correctamente')
+        fs.readFile(`./scores/${file}`,'utf-8',(err,data)=>{
+            if(err){
+                throw new Error('ocurrio un error al leer el archivo')
+            }
+            const winners = convertStringIntoObject(data)
+            const maxDifference = Math.max(...Object.keys(winners))
+            const output = `${winners[maxDifference][2] } ${maxDifference} `
+            
+            fs.writeFile(`./scores/results.txt`, output,  (err) => {
+                if (err) throw new Error('ocurrio un error al crear el archivo con los resultados');
+                    console.log('Guardamos los resultados correctamente');
+              }); 
+        })
     })
-})
+}
+whoIsTheWinner('score1.txt')
